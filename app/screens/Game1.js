1ceb59898af5478c06b9b3d98e26834a276dda1d
Namishk <namihkhandelwal1@gmail.com>
2021-08-08 22:08:15 +0530

    video added in game 1

--- app/screens/Game1.js
+++ app/screens/Game1.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-
+import {Video} from 'expo-av';
 
 
 const Game1 = (props) => {
 
 	return(
 		<View style = {Styles.container} >
-		<Text>Game should be here..</Text>
+		<Video
+			source={{uri : 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'}}
+			shouldPlay
+			resizeMode="cover"
+			useNativeControls
+			style={{ width: "100%", height: 300 }}
+		/>
 		</View>
 		)
 
